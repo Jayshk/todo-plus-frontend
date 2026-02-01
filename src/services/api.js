d15fdd33ed1e7dcb63e74cd7086185ca5af7eb63
jayshk <jayeshkushwah06@gmail.com>
2026-02-01 14:37:57 +0530

    rewire

--- src/services/api.js
+++ src/services/api.js
@@ -21,3 +21,36 @@ export async function apiFetch(path, options = {}) {
 
   return res.json();
 }
+
+// Todo-related API functions
+export async function getTodos() {
+  return apiFetch('/todos');
+}
+
+export async function createTodo(data) {
+  return apiFetch('/todos', {
+    method: 'POST',
+    body: JSON.stringify(data),
+  });
+}
+
+export async function updateTodo(id, data) {
+  return apiFetch(`/todos/${id}`, {
+    method: 'PUT',
+    body: JSON.stringify(data),
+  });
+}
+
+export async function deleteTodo(id) {
+  return apiFetch(`/todos/${id}`, {
+    method: 'DELETE',
+  });
+}
+
+// Auth-related API functions
+export async function login(email, password) {
+  return apiFetch('/auth/login', {
+    method: 'POST',
+    body: JSON.stringify({ email, password }),
+  });
+}
